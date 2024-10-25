205efa98869956dd58d8d55342731c2ee7607d19
Fedor Osetrov <33493672+fdr400@users.noreply.github.com>
2024-10-25 11:14:01 +0300

    Downgrade Kafka version to feat Ubuntu 24.04 default

--- cmake/SetupRdKafka.cmake
+++ cmake/SetupRdKafka.cmake
@@ -2,7 +2,7 @@ include_guard(GLOBAL)
 
 option(USERVER_DOWNLOAD_PACKAGE_KAFKA "Download and setup librdkafka if no librdkafka matching version was found" ${USERVER_DOWNLOAD_PACKAGES})
 
-set(USERVER_KAFKA_VERSION "2.4.0")
+set(USERVER_KAFKA_VERSION "2.3.0")
 
 find_package(OpenSSL COMPONENTS SSL Crypto REQUIRED)
 find_package(CURL REQUIRED)
