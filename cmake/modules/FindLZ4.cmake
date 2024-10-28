6a29385a2e5711f7ec4601ebcebd14f4568bc48c
Fedor <fiosetrova@gmail.com>
2024-10-28 06:56:57 +0000

    Find LZ4

---
+++ cmake/modules/FindLZ4.cmake
@@ -0,0 +1,18 @@
+_userver_module_begin(
+    NAME LZ4
+    VERSION 1.9.2
+    DEBIAN_NAMES liblz4-dev
+    FORMULA_NAMES lz4
+    PKG_CONFIG_NAMES liblz4
+)
+
+_userver_module_find_include(
+    NAMES lz4.h
+)
+
+_userver_module_find_library(
+    NAMES liblz4.a
+)
+
+_userver_module_end()
+
