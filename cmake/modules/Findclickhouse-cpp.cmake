35b42ac3736063fdafbd292e04c618d84d250614
fdr400 <fdr400@yandex-team.com>
2025-01-09 10:39:06 +0300

    Do not find libcityhash for Linux clickhouse

--- cmake/modules/Findclickhouse-cpp.cmake
+++ cmake/modules/Findclickhouse-cpp.cmake
@@ -11,15 +11,17 @@ _userver_module_find_include(
 )
 
 _userver_module_find_library(
-    NAMES libclickhouse-cpp-lib.a clickhouse-cpp-lib
+    NAMES clickhouse-cpp-lib-static clickhouse-cpp-lib
     PATH_SUFFIXES
       clickhouse-cpp
       yandex/clickhouse-cpp
 )
 
-_userver_module_find_library(
+if(CMAKE_SYSTEM_NAME MATCHES "Darwin")
+  _userver_module_find_library(
     NAMES libcityhash.a
     PATHS /opt/homebrew/opt/clickhouse-cpp/libexec/lib
-)
+  )
+endif()
 
 _userver_module_end()
